0d3e55d4e13214e2004d61ed86eaae88823e7dd4
Gwenaël <g.magnenat@gmail.com>
2020-12-27 15:30:16 +0100

    Create new react app with CRA and clean the project, remove logo, test files and non necessary CSS files

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyA-qcbfjDQ2qnfjKqC-XyKpVQCmGCDZQG4",
+    authDomain: "e-commerce-project-e3a9c.firebaseapp.com",
+    projectId: "e-commerce-project-e3a9c",
+    storageBucket: "e-commerce-project-e3a9c.appspot.com",
+    messagingSenderId: "789893050382",
+    appId: "1:789893050382:web:8117d99c3bcda283b94c5f"
+  };
